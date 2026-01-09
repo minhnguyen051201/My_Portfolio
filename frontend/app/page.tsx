746a7b5f3347f991a6c19b78954446c96eb56288
stuart <stuart@Stuart-Workplace.local>
2026-01-09 15:41:56 -0500

    LeftSidebar Initialize

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,11 @@
-import LeftSidebar from "../components/sidebar/LeftSidebar"
-import RightSidebar from "../components/sidebar/RightSidebar.tsx"
+import LeftSidebar from "../components/sidebar/LeftSidebar";
+import RightSidebar from "../components/sidebar/RightSidebar.tsx";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background text-foreground">
-      Hello from Minh Nguyen right now
-      <LeftSidebar/>
-      <RightSidebar/>
+    <main className="min-h-screen bg-background text-foreground grid min-h-[80vh] grid-cols-[120px_1fr]">
+      <LeftSidebar />
+      <RightSidebar />
     </main>
   );
 }
--- frontend/components/sidebar/LeftSidebar.tsx
+++ frontend/components/sidebar/LeftSidebar.tsx
@@ -1,7 +1,20 @@
+import { Github } from "lucide-react";
+
 const LeftSidebar = () => {
   return (
-    <div>Hello from leftsidebar</div>
-  )
-}
+    <aside className="flex flex-col justify-between align-center border-r-4 px-6 py-8">
+      <div className="font-heading text-2xl font-semibold">
+        Stu<span className="text-green-600">art</span>
+      </div>
+      <div className="flex flex-col justify-between align-center">
+        <div>
+          <Github />
+        </div>
+        <div>Github</div>
+        <div>Youtube</div>
+      </div>
+    </aside>
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
